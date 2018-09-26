7b6d629b301aa51882ec38ab50c730542a77163d
Sinat <kevinthuybui@gmail.com>
2018-09-25 19:34:14 -0700

    Added comments

--- src/TokenScanner.java
+++ src/TokenScanner.java
@@ -1,11 +1,10 @@
 import Tokens.Token;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- *  This class handles the parsing of code input and the creation of the token array.
+ *  This class handles the parsing of code input and the creation of the token output.
  *
  *  @author Stefan Brand
  *  @author Kevin Bui Kevinthuybui@gmail.com
@@ -32,7 +31,7 @@ public class TokenScanner {
      * The default constructor. Requires a <code>PushBackLineNumberReader</code>
      * @param code The input stream that contains the code to be parsed.
      */
-    public TokenScanner(PushBackLineNumberReader code) {
+    TokenScanner(PushBackLineNumberReader code) {
         this.code = code;
     }
     
@@ -52,50 +51,46 @@ public class TokenScanner {
         return character;
     }
     
-    /**
+    /** //TODO Finish this
      * Calls <code>parseNextToken()</code> to get a token then checks if characters need to be unread by comparing
-     * the value of the lastValidToken with the 
+     * the value of the lastValidToken with the
      * @return The parsed Token
      * @throws IOException Passes the exception from <code>code.peek()</code>
      */
     
     private Token getNextToken() throws IOException {
-        StringBuilder newTokenValueBuilder = new StringBuilder();   // This will hold the value of the new token
+        StringBuilder newTokenValueBuilder = new StringBuilder();
         Token lastValidToken = parseNextToken(newTokenValueBuilder);
-        
-        if (lastValidToken.getId() == 1){
-            currentState = State.START;
-            return lastValidToken;
-        }
+        handleUnusedCharacters(newTokenValueBuilder, lastValidToken);
+        currentState = State.START;
+        return lastValidToken;
+    }
     
-        /**
-         * Checks if the last valid token parsed consists of the entirety of the StringBuilder in order to check if
-         * the
-         */
-        if (lastValidToken.getCodeString().length() == newTokenValueBuilder.length()) {
-            currentState = State.START;
-            return lastValidToken;
-        } else {
+    /**
+     * Checks the length of <code>codeString</code> of the given token against the value of the StringBuilder to
+     * determine if too many characters have been read and <code>unread</code> the unneeded characters.
+     * @param newTokenValueBuilder The characters that have been read from input stream
+     * @param lastValidToken The last token that was in an accepting state
+     */
+    private void handleUnusedCharacters(StringBuilder newTokenValueBuilder, Token lastValidToken) {
+        if (lastValidToken.getCodeString().length() != newTokenValueBuilder.length()) {
             newTokenValueBuilder.delete(0, lastValidToken.getCodeString().length());
             code.unread(newTokenValueBuilder.toString());
-    
-            currentState = State.START;
-            return lastValidToken;
-    
         }
     }
     
+    /**
+     * The token is built a character at a time by passing the <code>CurrentSituation</code> object
+     * to the transitionMap. This serves as the key for the <code>HashMap</code> which returns the next
+     * state or null if invalid next state.
+     */
     private Token parseNextToken(StringBuilder newTokenValueBuilder) throws IOException {
         char nextChar = code.peek();
         Token lastValidToken = null;
         
         while(nextChar != '\n' && nextChar != ' '){ // Continue while the next character is not whitespace
             nextChar = advance();
-            /**
-             * The token is built a character at a time by passing the <code>CurrentSituation</code> object
-             * to the transitionMap. This serves as the key for the <code>HashMap</code> which returns the next
-             * state or null if invalid next state.
-             */
+            
             currentState = transitionMap.get(new CurrentSituation(currentState, nextChar));
             newTokenValueBuilder.append(nextChar);
         
@@ -106,7 +101,7 @@ public class TokenScanner {
                 lastValidToken = handleComment();
                 break;
             } else if (currentState == State.STRING) {
-                lastValidToken =  handleString();
+                lastValidToken =  createStringToken();
                 break;
             }
         
@@ -122,19 +117,36 @@ public class TokenScanner {
         return lastValidToken;
     }
     
-    private Token handleString() throws IOException {
+    /**
+     * Creates a String token
+     * @return String Token
+     * @throws IOException unhandled from processString()
+     */
+    private Token createStringToken() throws IOException {
         currentState = State.START;
         String stringTokenValue = processString();
         return TokenFactory.createToken(State.STRING, code.getLineNumber(), stringTokenValue);
     }
+    
+    /**
+     * Discards the remaining line from stdin then creates a comment token.
+     * @return Token A comment token
+     * @throws IOException from code.readChar()
+     */
     
     private Token handleComment() throws IOException {
         while (code.peek() != '\n') {
             code.readChar();
         }
         
-        return TokenFactory.createToken(State.COMMENT, 0, " ");
+        return TokenFactory.createToken(State.COMMENT, 0, "//");
     }
+    
+    /**
+     * Creates an <code>ArrayList</code> of <code>Token</code>. Adds an end of file token to the end of array.
+     * @return ArrayList of Tokens
+     * @throws IOException unhandled exception from getNextToken()
+     */
     
     public ArrayList<Token> getAllTokens() throws IOException {
         clearWhitespace();
@@ -149,10 +161,20 @@ public class TokenScanner {
         return allTokens;
     }
     
-    private void clearWhitespace() throws IOException {
+    /**
+     * Advances PushBackLineNumberReader to next non-whitespace character.
+     */
+    
+    private void clearWhitespace() {
         while (code.peek() == ' ' ||  code.peek() == '\n' )
             advance();
     }
+    
+    /**
+     * Creates a String from all characters until the next quote.
+     * @return the code string of the String Token
+     * @throws IOException Unhandled from readChar()
+     */
     
     private String processString() throws IOException {
         StringBuilder newString = new StringBuilder();
@@ -161,7 +183,7 @@ public class TokenScanner {
             newString.append(code.readChar());
             
         }
-        code.readChar();
+        code.readChar(); // Necessary to consume the second quote character.
         return newString.toString();
         
     }
