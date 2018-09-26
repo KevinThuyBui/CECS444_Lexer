848a712a80f3c082375a008c1aa191a528ce690f
m4rg4sh <epprest1@students.zhaw.ch>
2018-09-25 17:07:30 -0700

    added basic keyword and id handler

--- src/TokenFactory.java
+++ src/TokenFactory.java
@@ -9,6 +9,12 @@ public class TokenFactory
                 return new IntToken(3, lineNumber, tokenValue, Integer.parseInt(tokenValue));
             case FLOAT:
                 return new FloatToken(4, lineNumber, tokenValue, Float.parseFloat(tokenValue));
+            case KPROG:
+                return new Token(10,lineNumber,tokenValue);
+            case ID:
+                return new Token(2,lineNumber,tokenValue);
+            case COMMENT:
+                return null;
             default:
                 return new Token(99,lineNumber,tokenValue);
         }
